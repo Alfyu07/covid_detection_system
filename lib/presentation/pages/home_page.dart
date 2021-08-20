602bf0c9519a024f3a6ad68539b25f54370a80a5
Alfyu07 <w.alfa433@gmail.com>
2021-08-20 21:11:50 +0800

    refactor

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -41,120 +41,18 @@ class HomePage extends StatelessWidget {
               ),
             ),
             const SizedBox(height: edge),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: edge),
-              child: CarouselSlider(
-                items: const [
-                  CarouselItem(
-                    image: AssetImage('assets/banner 1.png'),
-                    title: 'Wear a mask - saves life',
-                    subtitle:
-                        'Did you know that wearing a mask can actually saves life?',
-                  ),
-                  CarouselItem(
-                    image: AssetImage('assets/banner 2.png'),
-                    title: 'Diagnose with AI',
-                    subtitle:
-                        'Detect your desease quickly using artificial intelligence model',
-                  )
-                ],
-                options: CarouselOptions(
-                  height: 110,
-                  viewportFraction: 1,
-                  enlargeCenterPage: true,
-                ),
-              ),
-            ),
+            buildCarousel(),
             const SizedBox(height: edge),
+            //! Diagnose
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: edge),
               child: Text('Diagnose', style: mediumFont.copyWith(fontSize: 18)),
             ),
             const SizedBox(height: 16),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: edge),
-              child: SizedBox(
-                height: 28,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    Container(
-                      height: 28,
-                      margin: const EdgeInsets.only(right: 16),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 4),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        color: primaryColor,
-                      ),
-                      child: Text('Terbaru',
-                          style: whiteMediumFont.copyWith(fontSize: 12)),
-                    ),
-                    Container(
-                      height: 28,
-                      margin: const EdgeInsets.only(right: 16),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 4),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: primaryColor, width: 1.5),
-                          borderRadius: BorderRadius.circular(16)),
-                      child: Text(
-                        'Covid 19',
-                        style: mediumFont.copyWith(
-                            fontSize: 12, color: primaryColor),
-                      ),
-                    ),
-                    Container(
-                      height: 28,
-                      margin: const EdgeInsets.only(right: 16),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 4),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: primaryColor, width: 1.5),
-                          borderRadius: BorderRadius.circular(16)),
-                      child: Text(
-                        'Pneumonia',
-                        style: mediumFont.copyWith(
-                            fontSize: 12, color: primaryColor),
-                      ),
-                    ),
-                    Container(
-                      height: 28,
-                      margin: const EdgeInsets.only(right: 16),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 4),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: primaryColor, width: 1.5),
-                          borderRadius: BorderRadius.circular(16)),
-                      child: Text(
-                        'Normal',
-                        style: mediumFont.copyWith(
-                            fontSize: 12, color: primaryColor),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            buildSort(),
             const SizedBox(height: 16),
-            Container(
-              height: 286,
-              color: whiteColor,
-              child: ListView.builder(
-                itemCount: diagnoses.length,
-                itemBuilder: (BuildContext context, int index) =>
-                    GestureDetector(
-                  onTap: () {
-                    // TODO Navigate to details
-                  },
-                  child: DiagnosisCard(
-                    diagnosis: diagnoses[index],
-                  ),
-                ),
-              ),
-            ),
+            buildDiagnoseList(),
             const SizedBox(height: 16),
-
             //! Disease
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: edge),
@@ -166,6 +64,112 @@ class HomePage extends StatelessWidget {
             const SizedBox(height: 8),
             buildDisease(),
             const SizedBox(height: 80),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding buildCarousel() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: edge),
+      child: CarouselSlider(
+        items: const [
+          CarouselItem(
+            image: AssetImage('assets/banner 1.png'),
+            title: 'Wear a mask - saves life',
+            subtitle:
+                'Did you know that wearing a mask can actually saves life?',
+          ),
+          CarouselItem(
+            image: AssetImage('assets/banner 2.png'),
+            title: 'Diagnose with AI',
+            subtitle:
+                'Detect your desease quickly using artificial intelligence model',
+          )
+        ],
+        options: CarouselOptions(
+          height: 110,
+          viewportFraction: 1,
+          enlargeCenterPage: true,
+        ),
+      ),
+    );
+  }
+
+  Widget buildDiagnoseList() {
+    return Container(
+      height: 286,
+      color: whiteColor,
+      child: ListView.builder(
+        itemCount: diagnoses.length,
+        itemBuilder: (BuildContext context, int index) => GestureDetector(
+          onTap: () {
+            // TODO Navigate to details
+          },
+          child: DiagnosisCard(
+            diagnosis: diagnoses[index],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildSort() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: edge),
+      child: SizedBox(
+        height: 28,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Container(
+              height: 28,
+              margin: const EdgeInsets.only(right: 16),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: primaryColor,
+              ),
+              child: Text('Terbaru',
+                  style: whiteMediumFont.copyWith(fontSize: 12)),
+            ),
+            Container(
+              height: 28,
+              margin: const EdgeInsets.only(right: 16),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+              decoration: BoxDecoration(
+                  border: Border.all(color: primaryColor, width: 1.5),
+                  borderRadius: BorderRadius.circular(16)),
+              child: Text(
+                'Covid 19',
+                style: mediumFont.copyWith(fontSize: 12, color: primaryColor),
+              ),
+            ),
+            Container(
+              height: 28,
+              margin: const EdgeInsets.only(right: 16),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+              decoration: BoxDecoration(
+                  border: Border.all(color: primaryColor, width: 1.5),
+                  borderRadius: BorderRadius.circular(16)),
+              child: Text(
+                'Pneumonia',
+                style: mediumFont.copyWith(fontSize: 12, color: primaryColor),
+              ),
+            ),
+            Container(
+              height: 28,
+              margin: const EdgeInsets.only(right: 16),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+              decoration: BoxDecoration(
+                  border: Border.all(color: primaryColor, width: 1.5),
+                  borderRadius: BorderRadius.circular(16)),
+              child: Text(
+                'Normal',
+                style: mediumFont.copyWith(fontSize: 12, color: primaryColor),
+              ),
+            ),
           ],
         ),
       ),
